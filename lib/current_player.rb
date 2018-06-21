78f36e053e306af08f153988674757eb7ab46e54
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-21 04:02:45 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+#takes argument of board array
+#returns number of turns that have elapsed
+
+def turn_count(board)
+  counter = 0
+  board.each do |item|
+  puts {item}
+   if (item != " ")
+   counter += 1
+    end
+  end
+  return counter
+end
